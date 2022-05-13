871944fa1eabc45ce317aaf4ae56b02933a8409e
microdotmatrix <liquid.knives.2@gmail.com>
2022-05-13 08:00:41 -0500

    pst

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,3 @@ window.jQuery = jQuery;
 //import luge from '@waaark/luge'
 import { gsap, ScrollTrigger, Flip } from "gsap/all";
 
-// Just notating something in case I need it, will delete later...
-//Original DNS Records (A) on Wix:
-//
-//	185.230.63.171
-//  185.230.63.186
-//  185.230.63.107
